f49d8ef95fdaf01384713fbd3ea207407b0c6e07
Trisha Pal <110612683+trisha194@users.noreply.github.com>
2025-01-23 20:41:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,5 +65,10 @@ def index():
 
     return render_template("index.html", prediction=None)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use $PORT if available, otherwise default to 5000
+    app.run(host="0.0.0.0", port=port)
+
+
